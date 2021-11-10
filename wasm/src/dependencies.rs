c8f0903fcbac67629b3731dc639ffa96868a9b5b
Ivan Schuetz <ivanschuetz@gmail.com>
2021-11-10 17:40:13 +0100

    Sandbox default connection data

--- wasm/src/dependencies.rs
+++ wasm/src/dependencies.rs
@@ -49,8 +49,8 @@ fn testnet_algod(api_key: &str) -> Algod {
 #[allow(dead_code)]
 fn private_network_algod() -> Algod {
     AlgodBuilder::new()
-        .bind("http://127.0.0.1:53630")
-        .auth("44d70009a00561fe340b2584a9f2adc6fec6a16322554d44f56bef9e682844b9")
+        .bind("http://127.0.0.1:4001")
+        .auth("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         .build_v2()
         // expect: build returns an error if the URL or token are not provided or have an invalid format,
         // we are passing verified hardcoded values.
